48709af138536ec864b15965405861e4da348b37
Hevin <hevinvnsgu@gmail.com>
2024-07-20 16:01:20 +0530

    refactor: remove unused footer code in site-footer.tsx

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -10,20 +10,6 @@ export function SiteFooter() {
             "radial-gradient(50% 100% at 50% 100%,rgba(255,255,255,.32) 0%,rgba(255,255,255,0) 100%)",
         }}
       ></div>
-      <div className="container flex flex-col items-center justify-between gap-4 md:h-16 md:flex-row">
-        <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-          Brought to you by{" "}
-          <a
-            href={siteConfig.links.twitter}
-            target="_blank"
-            rel="noreferrer"
-            className="font-medium underline underline-offset-4"
-          >
-            dillion
-          </a>
-          .
-        </p>
-      </div>
     </footer>
   );
 }

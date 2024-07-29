851e8370507bafa0c150f73733a77ae173ab63b7
hevinkalathiya <hevinvnsgu@gmail.com>
2024-07-29 17:53:37 +0530

    feat: add number ticker component for animated percentage values

--- registry/components/example/states/states-one.tsx
+++ registry/components/example/states/states-one.tsx
@@ -1,21 +1,25 @@
+import { useEffect, useRef } from "react";
+import { useInView, useMotionValue, useSpring } from "framer-motion";
 import { ArrowRight, Zap } from "lucide-react";
+
+import { cn } from "@/lib/utils";
 
 const State = () => {
   const stats = [
     {
-      percentage: "100%",
+      percentage: 100,
       description: "Vocals",
     },
     {
-      percentage: "90%",
+      percentage: 90,
       description: "Dancing",
     },
     {
-      percentage: "95%",
+      percentage: 95,
       description: "Performing",
     },
     {
-      percentage: "100%",
+      percentage: 100,
       description: "Beauty",
     },
   ];
@@ -42,11 +46,13 @@ const State = () => {
           <div key={index} className="flex items-center gap-5">
             <div className="w-0.5 h-28 bg-gradient-to-b from-transparent via-purple-600 to-transparent" />
             <div>
-              <div className="text-lg md:text-3xl">{stat.percentage}</div>
-              <div className="mt-2 text-neutral-600 dark:text-zinc-300 max-w-36">
+              <div className="text-3xl">
+                <NumberTicker value={stat.percentage} />%
+              </div>
+              <div className="mt-1 md:mt-2 text-neutral-600 dark:text-zinc-300 max-w-36">
                 {stat.description}
               </div>
-              <div className="flex items-center gap-2 cursor-pointer group mt-3">
+              <div className="flex items-center gap-2 cursor-pointer group mt-2 md:mt-3 text-sm">
                 Learn More
                 <ArrowRight className="size-4 transition-transform group-hover:translate-x-1" />
               </div>
@@ -59,3 +65,53 @@ const State = () => {
 };
 
 export default State;
+
+// Thank you magic-ui for the NumberTicker component
+function NumberTicker({
+  value,
+  direction = "up",
+  delay = 0,
+  className,
+}: {
+  value: number;
+  direction?: "up" | "down";
+  className?: string;
+  delay?: number; // delay in s
+}) {
+  const ref = useRef<HTMLSpanElement>(null);
+  const motionValue = useMotionValue(direction === "down" ? value : 0);
+  const springValue = useSpring(motionValue, {
+    damping: 60,
+    stiffness: 100,
+  });
+  const isInView = useInView(ref, { once: true, margin: "0px" });
+
+  useEffect(() => {
+    isInView &&
+      setTimeout(() => {
+        motionValue.set(direction === "down" ? 0 : value);
+      }, delay * 1000);
+  }, [motionValue, isInView, delay, value, direction]);
+
+  useEffect(
+    () =>
+      springValue.on("change", (latest) => {
+        if (ref.current) {
+          ref.current.textContent = Intl.NumberFormat("en-US").format(
+            latest.toFixed(0),
+          );
+        }
+      }),
+    [springValue],
+  );
+
+  return (
+    <span
+      className={cn(
+        "inline-block tabular-nums text-black dark:text-white tracking-wider",
+        className,
+      )}
+      ref={ref}
+    />
+  );
+}

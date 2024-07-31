85f3836f66a3a29991c2e19e2e77d35a4400bf06
Hevin <hevinvnsgu@gmail.com>
2024-07-31 21:15:32 +0530

    refactor: update dashboard image source and quality

--- registry/components/example/states/states-three.tsx
+++ registry/components/example/states/states-three.tsx
@@ -4,10 +4,11 @@ const States = () => {
   return (
     <div className="block md:flex items-center">
       <Image
-        src="/public/dashboard.png"
+        src="/dashboard.png"
         alt="dashboard"
         height={500}
         width={500}
+        quality={100}
       />
     </div>
   );

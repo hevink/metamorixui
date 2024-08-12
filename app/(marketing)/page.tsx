d115bac231d723db8794683bb5cfc51e33978c67
Hevin <hevinvnsgu@gmail.com>
2024-08-12 16:50:11 +0530

    feat: add hero

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,50 +1,11 @@
 "use client";
 
-import { useEffect, useState } from "react";
-
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
+import SiteHero from "@/components/lp/SiteHero";
 
 export default function Home() {
-  const [gradientIndex, setGradientIndex] = useState(0);
-
-  const gradients = [
-    "from-violet-600 to-purple-600",
-    "from-blue-600 to-cyan-600",
-    "from-green-600 to-emerald-600",
-    "from-yellow-600 to-orange-600",
-    "from-red-600 to-pink-600",
-  ];
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setGradientIndex((prevIndex) => (prevIndex + 1) % gradients.length);
-    }, 1500); // Change every 1.5 seconds
-
-    return () => clearInterval(intervalId);
-  }, []);
-
   return (
-    <div className="flex items-center justify-center h-[calc(100vh-110px)] relative">
-      <div
-        className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[600px] h-32 bg-gradient-to-br ${gradients[gradientIndex]} rounded-3xl blur-[200px] -z-50 transition-colors duration-1000`}
-      />
-
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <p
-            className={`bg-gradient-to-r ${gradients[gradientIndex]}  bg-clip-text text-transparent text-4xl font-bold`}
-          >
-            Rukja ja bhai abhi banaunga
-          </p>
-        </TooltipTrigger>
-        <TooltipContent>
-          <p>Work in progress brother 😊</p>
-        </TooltipContent>
-      </Tooltip>
+    <div className="relative flex h-[calc(100vh-110px)] items-center justify-center">
+      <SiteHero />
     </div>
   );
 }
--- components/lp/SiteHero.tsx
+++ components/lp/SiteHero.tsx
@@ -1,8 +1,5 @@
-import React from "react";
 import Link from "next/link";
 import { ArrowRight, ChevronRight } from "lucide-react";
-
-import { cn } from "@/lib/utils";
 
 const SiteHero = () => {
   return (
@@ -56,51 +53,3 @@ const SiteHero = () => {
 };
 
 export default SiteHero;
-
-interface BorderBeamProps {
-  className?: string;
-  size?: number;
-  duration?: number;
-  borderWidth?: number;
-  anchor?: number;
-  colorFrom?: string;
-  colorTo?: string;
-  delay?: number;
-}
-
-const BorderBeam = ({
-  className,
-  size = 200,
-  duration = 15,
-  anchor = 90,
-  borderWidth = 2,
-  colorFrom = "#0000ff",
-  colorTo = "#7800ff",
-  delay = 0,
-}: BorderBeamProps) => {
-  return (
-    <div
-      style={
-        {
-          "--size": size,
-          "--duration": duration,
-          "--anchor": anchor,
-          "--border-width": borderWidth,
-          "--color-from": colorFrom,
-          "--color-to": colorTo,
-          "--delay": `-${delay}s`,
-        } as React.CSSProperties
-      }
-      className={cn(
-        "absolute inset-0 rounded-[inherit] [border:calc(var(--border-width)*1px)_solid_transparent]",
-
-        // mask styles
-        "![mask-clip:padding-box,border-box] ![mask-composite:intersect] [mask:linear-gradient(transparent,transparent),linear-gradient(white,white)]",
-
-        // pseudo styles
-        "after:absolute after:aspect-square after:w-[calc(var(--size)*1px)] after:animate-border-beam after:[animation-delay:var(--delay)] after:[background:linear-gradient(to_left,var(--color-from),var(--color-to),transparent)] after:[offset-anchor:calc(var(--anchor)*1%)_50%] after:[offset-path:rect(0_auto_auto_0_round_calc(var(--size)*1px))]",
-        className,
-      )}
-    />
-  );
-};

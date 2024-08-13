3a097ff74abff5cc503c36a2394427920156701d
Sujal <sujalthummar23@gmail.com>
2024-08-13 23:22:58 +0530

    refactor: update ui in testimonial second components

--- registry/components/example/testimonial/testimonial-two.tsx
+++ registry/components/example/testimonial/testimonial-two.tsx
@@ -2,30 +2,30 @@ import React from "react";
 
 const Testimonial = () => {
   return (
-    <section className="min-w-screen min-h-screen flex items-center justify-center sm:py-5">
-      <div className="w-full border-gray-200 dark:border-gray-700 sm:px-5 py-16 md:py-24 text-gray-800">
-        <div className="w-full max-w-6xl mx-auto">
-          <div className="text-center max-w-xl mx-auto">
-            <h1 className="text-6xl md:text-7xl font-bold mb-5 text-gray-800 dark:text-gray-200">
+    <section className="min-w-screen flex min-h-screen items-center justify-center sm:py-5">
+      <div className="w-full py-16 text-gray-800 sm:px-5 md:py-24">
+        <div className="mx-auto w-full max-w-6xl">
+          <div className="mx-auto max-w-xl text-center">
+            <h1 className="mb-5 text-6xl font-bold text-gray-800 dark:text-gray-200 md:text-7xl">
               What people <br />
-              <span className="text-transparent bg-clip-text bg-gradient-to-r from-orange-500 to-orange-600">
+              <span className="bg-gradient-to-r from-orange-500 to-orange-600 bg-clip-text text-transparent">
                 are saying.
               </span>
             </h1>
-            <h3 className="text-xl mb-5 font-light text-gray-800 dark:text-gray-200">
+            <h3 className="mb-5 text-xl font-light text-gray-800 dark:text-gray-200">
               Check out what our customers have to say about us!
             </h3>
-            <div className="text-center mb-10">
-              <span className="inline-block w-1 h-1 rounded-full bg-[#FE6019] ml-1"></span>
-              <span className="inline-block w-3 h-1 rounded-full bg-[#FE6019] ml-1"></span>
-              <span className="inline-block w-40 h-1 rounded-full bg-[#FE6019]"></span>
-              <span className="inline-block w-3 h-1 rounded-full bg-[#FE6019] ml-1"></span>
-              <span className="inline-block w-1 h-1 rounded-full bg-[#FE6019] ml-1"></span>
+            <div className="mb-10 text-center">
+              <span className="ml-1 inline-block h-1 w-1 rounded-full bg-[#FE6019]"></span>
+              <span className="ml-1 inline-block h-1 w-3 rounded-full bg-[#FE6019]"></span>
+              <span className="inline-block h-1 w-40 rounded-full bg-[#FE6019]"></span>
+              <span className="ml-1 inline-block h-1 w-3 rounded-full bg-[#FE6019]"></span>
+              <span className="ml-1 inline-block h-1 w-1 rounded-full bg-[#FE6019]"></span>
             </div>
           </div>
-          <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-2 sm:gap-4 md:gap-6 !gap-y-0">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
             {testimonials.map((testimonial, index) => (
-              <TestimonialCard key={index} {...testimonial} />
+              <TestimonialCard key={index} {...testimonial} index={index} />
             ))}
           </div>
         </div>
@@ -36,29 +36,32 @@ const Testimonial = () => {
 
 export default Testimonial;
 
-const TestimonialCard = ({ name, image, description }: any) => {
+const TestimonialCard = ({ name, image, description, index }: any) => {
   return (
-    <div className="relative w-full mx-auto rounded-lg border p-5 text-gray-800 font-light mb-6 transition-transform duration-300 ease-in-out transform hover:scale-105">
-      <div className="w-full flex mb-4 items-center">
-        <div className="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200 dark:border-gray-500">
-          <img src={image} alt={name} />
-        </div>
-        <div className="flex-grow pl-3">
-          <h6 className="font-bold text-sm uppercase text-gray-800 dark:text-gray-200">
-            {name}
-          </h6>
-        </div>
-      </div>
-      <div className="w-full">
-        <p className="text-sm leading-tight text-gray-800 dark:text-gray-200">
-          <span className="text-lg leading-none italic font-bold text-gray-400 mr-1">
-            "
-          </span>
+    <div
+      className={`dark:border-gray-850 w-full p-5 font-light text-gray-800 ${index === 0 || index === 1 ? "border-b border-r" : index === 2 ? "border-b sm:border-r" : index === 3 || index === 4 ? "border-b sm:border-r" : ""}`}
+    >
+      <div className="flex h-full flex-col justify-between">
+        <p className="mb-6 text-base leading-snug text-gray-800 dark:text-gray-400">
           {description}
-          <span className="text-lg leading-none italic font-bold text-gray-400 ml-1">
-            "
-          </span>
         </p>
+        <div className="mt-4 flex items-center">
+          <div className="h-10 w-10 overflow-hidden rounded-full border-2 border-gray-500 dark:border-gray-700">
+            <img
+              src={image}
+              alt={name}
+              className="h-full w-full object-cover"
+            />
+          </div>
+          <div className="ml-3">
+            <h6 className="text-base font-bold text-gray-800 dark:text-gray-200">
+              {name}
+            </h6>
+            <span className="text-sm text-gray-600 dark:text-gray-400">
+              Position @Company
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -69,36 +72,36 @@ const testimonials = [
     name: "Kenzie Edgar",
     image: "https://i.pravatar.cc/100?img=1",
     description:
-      "I've been using this product for several months now, and I must say, it has exceeded my expectations in every way possible. The quality is outstanding, and the customer service is second to none!",
+      "Working with Syed was a great experience. His drive for clean aesthetics and his open communication style helped us collaborate closely on the solution. He's the only designer I know who also has a deep understanding for development.",
   },
   {
     name: "Stevie Tifft",
     image: "https://i.pravatar.cc/100?img=2",
     description:
-      "Amazing service! The team went above and beyond to help me with my issue. I highly recommend this company to anyone looking for reliability and excellent support.",
+      "Syed is a fantastic UI/UX designer and developer with a keen sense for marketing, and lead generation. He's the only designer I know who also has a deep understanding for code and front-end development.",
   },
   {
     name: "Tommie Ewart",
     image: "https://i.pravatar.cc/100?img=3",
     description:
-      "This product has completely transformed how I work. It's intuitive, fast, and reliable. I can't imagine going back to anything else.",
+      "Talented artist, experienced with various media and can make any project you are working on look good. He is easy to work with and is a self-starter. I would work with Syed again.",
   },
   {
     name: "Charlie Howse",
     image: "https://i.pravatar.cc/100?img=4",
     description:
-      "I was skeptical at first, but after using this product for a few weeks, I'm a believer. It's simply the best out there.",
+      "Syed is a true professional with amazing talent. He is a creative thinker, has his finger on the pulse of what's to come, and absolutely one of the quickest and best problem solvers I have ever met.",
   },
   {
     name: "Nevada Herbertson",
     image: "https://i.pravatar.cc/100?img=5",
     description:
-      "The customer support is phenomenal. They were able to resolve my issue quickly and professionally. I'm impressed!",
+      "Syed is one of my favorite people to work with. He's my go-to colleague when troubleshooting any new technique or technology because he understands how to tackle unknown problems with gumption.",
   },
   {
     name: "Kris Stanton",
     image: "https://i.pravatar.cc/100?img=6",
     description:
-      "Top-notch product! It's rare to find something that works so well without any hiccups. Highly recommended.",
+      "Syed is a talented artist. He is very experienced with various media and can make any project you are working on look good. He is easy to work with and is a self-starter. I would work with Syed again.",
   },
 ];

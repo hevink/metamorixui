a4072c8c34f8a23ab5ecd2ac33d27cb651c15e94
Hevin <hevinvnsgu@gmail.com>
2024-08-14 20:32:30 +0530

    feat: improve badge components and add new features

--- registry/components/example/badge/badge-four.tsx
+++ registry/components/example/badge/badge-four.tsx
@@ -1,5 +1,35 @@
+import { motion } from "framer-motion";
+import { ArrowRight } from "lucide-react";
+
 const Badge = () => {
-  return <div></div>;
+  return (
+    <section className="flex items-center justify-center">
+      <div className="inline-flex items-center rounded-full border border-neutral-300 bg-neutral-200 p-1 dark:border-neutral-700 dark:bg-neutral-900">
+        <span className="ml-0.5 flex items-center justify-center rounded-full border border-neutral-300 bg-neutral-100 p-1.5 font-sans text-[9px] font-semibold uppercase leading-none tracking-widest dark:border-neutral-700 dark:bg-neutral-900">
+          New
+        </span>
+        <motion.span
+          initial={{ "--x": "100%" } as any}
+          animate={{ "--x": "-100%" } as any}
+          transition={{
+            repeat: Infinity,
+            repeatType: "loop",
+            duration: 2,
+            ease: "linear",
+          }}
+          className="px-2 text-sm [mask-image:linear-gradient(-75deg,#8a2be2_calc(var(--x)_+_20%),transparent_calc(var(--x)_+_30%),#8a2be2_calc(var(--x)_+_100%))] dark:[mask-image:linear-gradient(-75deg,#6c63ff_calc(var(--x)_+_20%),transparent_calc(var(--x)_+_30%),#6c63ff_calc(var(--x)_+_100%))]"
+        >
+          Introducing Metamorix UI 🐬
+        </motion.span>
+        <a
+          href="#"
+          className="flex cursor-pointer items-center justify-center gap-1 rounded-full border border-neutral-300 bg-neutral-100 p-1 dark:border-neutral-700 dark:bg-neutral-500/20"
+        >
+          <ArrowRight size={16} className="ml-px" />
+        </a>
+      </div>
+    </section>
+  );
 };
 
 export default Badge;
--- registry/components/example/badge/badge-three.tsx
+++ registry/components/example/badge/badge-three.tsx
@@ -1,5 +1,24 @@
+import { ArrowRight } from "lucide-react";
+
 const Badge = () => {
-  return <div></div>;
+  return (
+    <section className="flex items-center justify-center">
+      <div className="inline-flex items-center rounded-xl border border-neutral-300 bg-gray-200 p-1 dark:border-neutral-700 dark:bg-neutral-900">
+        <span className="mx-1 flex size-2 md:mx-3">
+          <span className="absolute inline-flex size-2 animate-ping rounded-full bg-purple-400 opacity-75" />
+          <span className="relative inline-flex size-2 rounded-full bg-purple-500" />
+        </span>
+        <span className="text-sm">Introducing Metamorix UI 🐬</span>
+        <a
+          href="#"
+          className="ml-3 flex cursor-pointer items-center justify-center gap-1 rounded-lg border border-neutral-300 bg-neutral-100 px-1 py-0.5 text-sm dark:border-neutral-700 dark:bg-neutral-500/20"
+        >
+          Get started
+          <ArrowRight size={14} className="ml-px" />
+        </a>
+      </div>
+    </section>
+  );
 };
 
 export default Badge;

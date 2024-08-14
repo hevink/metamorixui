03cedf77260b49a87274ee3e090bc88a01c32074
Hevin <hevinvnsgu@gmail.com>
2024-08-14 14:13:54 +0530

    fix: states

--- registry/components/example/states/states-two.tsx
+++ registry/components/example/states/states-two.tsx
@@ -7,8 +7,8 @@ interface StepItemProps {
 
 const Steps = () => {
   return (
-    <div className="mx-auto px-4 sm:px-6 lg:px-8 pb-16 sm:py-16 lg:pb-32 relative max-w-7xl">
-      <div className="grid sm:grid-cols-4 gap-y-8">
+    <div className="relative mx-auto max-w-7xl px-4 py-16">
+      <div className="grid gap-y-8 sm:grid-cols-4">
         <StepItem number={1}>
           Log in with your <strong>GitHub account</strong>
         </StepItem>
@@ -29,17 +29,13 @@ const Steps = () => {
 };
 
 const StepItem: React.FC<StepItemProps> = ({ number, children }) => (
-  <div className="relative group">
-    <div
-      className="absolute h-px hidden sm:block group-last:hidden top-[20px] left-[calc(50%+20px)] w-[calc(100%-40px)]"
-      style={{
-        backgroundImage: "linear-gradient(to right, #18181b, #3F3F46, #18181b)",
-      }}
-    ></div>
-    <div className="relative rounded-lg flex items-center justify-center mb-4 size-10 border border-gray-300 shrink-0 mx-auto font-semibold text-gray-900 dark:text-gray-300">
+  <div className="group relative">
+    <div className="absolute left-[calc(50%+20px)] top-[20px] hidden h-px w-[calc(100%-40px)] bg-gradient-to-r from-[#e5e7eb] via-[#9ca3af] to-[#e5e7eb] group-last:hidden dark:from-[#18181b] dark:via-[#3F3F46] dark:to-[#18181b]  sm:block"></div>
+
+    <div className="relative mx-auto mb-4 flex size-10 shrink-0 items-center justify-center rounded-lg border border-gray-300 dark:border-neutral-600 font-semibold text-gray-900 dark:text-gray-300">
       {number}
     </div>
-    <p className="mt-2 text-gray-600 dark:text-gray-400 text-center max-w-48 mx-auto prose prose-primary dark:prose-invert prose-sm">
+    <p className="prose-primary prose prose-sm mx-auto mt-2 max-w-48 text-center text-gray-600 dark:prose-invert dark:text-gray-400">
       {children}
     </p>
   </div>

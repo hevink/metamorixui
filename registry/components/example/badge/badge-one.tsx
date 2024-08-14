6a4e1db71265d859c21be449063fe200007cafd5
Hevin <hevinvnsgu@gmail.com>
2024-08-14 19:18:49 +0530

    feat: add second badge

--- registry/components/example/badge/badge-one.tsx
+++ registry/components/example/badge/badge-one.tsx
@@ -3,8 +3,8 @@ import { ArrowRight } from "lucide-react";
 const Badge = () => {
   return (
     <section className="flex items-center justify-center">
-      <div className="inline-flex items-center rounded-full border border-neutral-300 bg-gray-200 p-1 pr-3 dark:border-neutral-700 dark:bg-[#1f242f]">
-        <span className="mr-2 rounded-full border border-neutral-300 bg-white px-3 py-1 text-sm font-semibold dark:border-neutral-700 dark:bg-[#09090b]">
+      <div className="inline-flex items-center rounded-full border border-neutral-300 bg-gray-200 p-1 pr-3 dark:border-neutral-700 dark:bg-neutral-900">
+        <span className="mr-2 rounded-full border border-neutral-300 bg-white px-3 py-1 text-sm font-semibold dark:border-neutral-700 dark:bg-neutral-500/10">
           New
         </span>
         <span className="text-sm">Introducing Metamorix UI 🐬</span>
--- registry/components/example/badge/badge-two.tsx
+++ registry/components/example/badge/badge-two.tsx
@@ -1,7 +1,17 @@
+import { ArrowRight } from "lucide-react";
+
 const Badge = () => {
-  return <div>
-    
-  </div>;
+  return (
+    <section className="flex items-center justify-center">
+      <div className="inline-flex items-center rounded-full border border-violet-600 bg-violet-100 dark:bg-violet-500/20 p-1">
+        <span className="text-sm px-2">Introducing Metamorix UI 🐬</span>
+        <a href="#" className="flex items-center justify-center gap-1 rounded-full border border-violet-600 bg-violet-100 dark:bg-violet-500/20 px-3 py-1 text-sm font-semibold cursor-pointer">
+          Get started
+          <ArrowRight size={16} className="ml-px" />
+        </a>
+      </div>
+    </section>
+  );
 };
 
 export default Badge;

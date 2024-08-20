3349389be57346ad879ed5064176822d5edf4c83
Hevin <hevinvnsgu@gmail.com>
2024-08-20 17:08:00 +0530

    refactor: update page layout and styling

--- app/(docs)/docs/layout.tsx
+++ app/(docs)/docs/layout.tsx
@@ -10,7 +10,7 @@ export default function DocsLayout({ children }: DocsLayoutProps) {
   return (
     <div className="container flex-1 items-start md:grid md:grid-cols-[170px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[190px_minmax(0,1fr)] lg:gap-10">
       <aside className="fixed top-14 z-30 -ml-2 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 md:sticky md:block">
-        <ScrollArea className="h-full pr-6">
+        <ScrollArea className="h-full pr-2">
           <DocsSidebarNav items={docsConfig.sidebarNav} />
         </ScrollArea>
       </aside>

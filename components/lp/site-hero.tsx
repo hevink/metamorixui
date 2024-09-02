7af64bc5a2cc805a7bc6f2d1c4c510a937ee2435
hevinkalathiya <hevinvnsgu@gmail.com>
2024-09-02 14:35:06 +0530

    refactor: remove LogoCloud component from SiteHero

--- components/lp/site-hero.tsx
+++ components/lp/site-hero.tsx
@@ -94,78 +94,11 @@ const SiteHero = () => {
           </motion.div>
         </motion.div>
       </div>
-      <LogoCloud />
     </motion.section>
   );
 };
 
 export default SiteHero;
-
-const logos = [
-  {
-    name: "Babel",
-    url: "https://svgl.app/library/babel.svg",
-  },
-  {
-    name: "Ngrok",
-    url: "https://svgl.app/library/ngrok-light.svg",
-  },
-  {
-    name: "Webflow",
-    url: "https://svgl.app/library/webflow.svg",
-  },
-  {
-    name: "Perplexity",
-    url: "https://svgl.app/library/perplexity_wordmark_light.svg",
-  },
-  {
-    name: "Sanity",
-    url: "https://svgl.app/library/sanity.svg",
-  },
-  {
-    name: "Post CSS",
-    url: "https://svgl.app/library/postcss_wordmark.svg",
-  },
-];
-
-const LogoCloud = () => {
-  return (
-    <motion.div
-      initial={{ scale: 0.9, opacity: 0, filter: "blur(10px)" }}
-      animate={{ scale: 1, opacity: 1, filter: "blur(0px)" }}
-      transition={{ duration: 0.5, delay: 0.6 }}
-      className="w-full py-12"
-    >
-      <div className="mx-auto w-full px-2 md:px-4">
-        <div
-          className="group relative mt-6 flex gap-3 overflow-hidden p-2"
-          style={{
-            maskImage:
-              "linear-gradient(to left, transparent 0%, black 20%, black 80%, transparent 95%)",
-          }}
-        >
-          {Array(5)
-            .fill(null)
-            .map((index) => (
-              <div
-                key={index}
-                className="flex shrink-0 animate-logo-cloud flex-row justify-around gap-3"
-              >
-                {logos.map((logo, key) => (
-                  <img
-                    key={key}
-                    src={logo.url}
-                    className="h-20 w-40 rounded-md bg-neutral-500/10 px-5 py-1 dark:bg-neutral-950"
-                    alt={logo.name}
-                  />
-                ))}
-              </div>
-            ))}
-        </div>
-      </div>
-    </motion.div>
-  );
-};
 
 interface MousePosition {
   x: number;
